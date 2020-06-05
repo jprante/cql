dbed0211042eb30d1732d285ab8bc480dfd8ee0d
Jörg Prante <joergprante@gmail.com>
2020-06-05 11:18:47 +0200

    update to content 2.3.1

--- cql-elasticsearch/src/main/java/module-info.java
+++ cql-elasticsearch/src/main/java/module-info.java
@@ -2,6 +2,6 @@ module org.xbib.cql.elasticsearch {
     exports org.xbib.cql.elasticsearch;
     exports org.xbib.cql.elasticsearch.ast;
     exports org.xbib.cql.elasticsearch.model;
-    requires org.xbib.cql;
-    requires org.xbib.content.core;
+    requires transitive org.xbib.cql;
+    requires transitive org.xbib.content.core;
 }
